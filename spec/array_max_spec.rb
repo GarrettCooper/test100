6123bbe06e1b72a3e56a266381f5d9c9a57d2e00
Garrett Cooper <garrett.l.cooper@gmail.com>
2017-10-23 11:39:03 -0400

    updated rspec file

--- spec/array_max_spec.rb
+++ spec/array_max_spec.rb
@@ -1,9 +1,9 @@
 require "array_max"
 
     describe ".max_array" do
-        context "when user enters array [1,5,3,2,10,4]" do
-            it "should be 10" do
-                expect(max_array([1,5,3,2,10,4])).to be_truthy
+        context "when user enters array" do
+            it "should return maximum value" do
+                expect(max_array([1,5,3,2,10,4])).to eql(10)
             end
         end
     end
